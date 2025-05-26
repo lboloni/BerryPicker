9e3b24d126d49c1a2bd7acbcda2b61b441293875
Lotzi Bölöni <lotzi.boloni@gmail.com>
2025-05-26 18:03:41 -0400

    Converted Train and Verify Visual Proprio notebooks to new model

--- src/visual_proprioception/visproprio_helper.py
+++ src/visual_proprioception/visproprio_helper.py
@@ -10,82 +10,54 @@ Config.PROJECTNAME = "BerryPicker"
 import pathlib
 import torch
 import numpy as np
+from demonstration.demonstration import Demonstration
 # from demonstration.encoded_demonstrations import BCDemonstration
 
 from robot.al5d_position_controller import RobotPosition
 from sensorprocessing import sp_conv_vae, sp_propriotuned_cnn, sp_aruco, sp_vit, sp_vit_multiview, sp_vit_concat_images
+import sensorprocessing.sp_helper as sp_helper
 
-
-
-def load_demonstrations_as_proprioception_training_old(sp, task, proprioception_input_file, proprioception_target_file):
-    """
-    FIXME: it is not clear if this is the one where this data is put together - if that is the case, this is actually not the right function to call
-
-    Iterates over all the demonstrations of a task. Loads the images and the corresponding positions. Preprocesses the images with the sensor processor sp passed. Saves these data into proprioception_input_file, proprioception_target_file.
-
-    These data are cached, if the files exists, it does not iterate over the demonstrations, just loads the cached data. Remove those files to recalculate.
-
-    Shuffles the dataset thus created, and splits it into training and validation data.
-
-    Returns a dictionary with the training and validation data.
-    """
-    retval = {}
+def load_demonstrations_as_proprioception_training(sp, exp, spexp, datasetname, proprioception_input_file, proprioception_target_file, device=None):
+    """Loads all the images from the specified dataset and creates the input and target tensors. """
     if proprioception_input_file.exists():
+        retval = {}        
         retval["inputs"] = torch.load(proprioception_input_file, weights_only=True)
         retval["targets"] = torch.load(proprioception_target_file, weights_only=True)
-    else:
-        demos_dir = pathlib.Path(Config()["demos"]["directory"])
-        task_dir = pathlib.Path(demos_dir, "demos", task)
-
-        inputlist = []
-        targetlist = []
-
-        for demo_dir in task_dir.iterdir():
-            if not demo_dir.is_dir():
-                pass
-            bcd = BCDemonstration(demo_dir, sensorprocessor=sp)
-            # print(bcd)
-            z, a = bcd.read_z_a()
-            # normalize the actions
-            #print(z.shape)
-            #print(a.shape)
-            anorm = np.zeros(a.shape, np.float32)
-            for i in range(a.shape[0]):
-                rp = RobotPosition.from_vector(a[i])
-                anorm[i,:] = rp.to_normalized_vector()
-            # FIXME the repeated name for inputs and targets
-            #print(z.shape)
-            #print(anorm.shape)
-
-            for i in range(z.shape[0]):
-                inp = torch.from_numpy(z[i])
-                tgt = torch.from_numpy(anorm[i])
-                inputlist.append(inp)
-                targetlist.append(tgt)
-
-        retval["inputs"] = torch.stack(inputlist)
-        retval["targets"] = torch.stack(targetlist)
-        torch.save(retval["inputs"], proprioception_input_file)
-        torch.save(retval["targets"], proprioception_target_file)
-
-        # Separate the training and validation data.
-        # We will be shuffling the demonstrations
-        length = retval["inputs"].size(0)
-        rows = torch.randperm(length)
-        shuffled_inputs = retval["inputs"][rows]
-        shuffled_targets = retval["targets"][rows]
-
-        training_size = int( length * 0.67 )
-        retval["inputs_training"] = shuffled_inputs[1:training_size]
-        retval["targets_training"] = shuffled_targets[1:training_size]
-
-        retval["inputs_validation"] = shuffled_inputs[training_size:]
-        retval["targets_validation"] = shuffled_targets[training_size:]
-
         return retval
+
+    inputlist = []
+    targetlist = []
+    transform = sp_helper.get_transform_to_sp(spexp)
+    
+    for val in exp[datasetname]:
+        run, demo_name, camera = val
+        exp_demo = Config().get_experiment("demonstration", run)
+        demo = Demonstration(exp_demo, demo_name)
+        for i in range(demo.metadata["maxsteps"]):
+            sensor_readings, _ = demo.get_image(i, camera=camera, transform=transform, device=device)
+            z = sp.process(sensor_readings)
+            a = demo.get_action(i)
+            #anorm = np.zeros(a.shape, np.float32)
+            rp = RobotPosition.from_vector(a)
+            anorm = rp.to_normalized_vector()
+            inp = torch.from_numpy(z)
+            tgt = torch.from_numpy(anorm)
+            inputlist.append(inp)
+            targetlist.append(tgt)
+    retval = {}
+    retval["inputs"] = torch.stack(inputlist)
+    retval["targets"] = torch.stack(targetlist)
+    torch.save(retval["inputs"], proprioception_input_file)
+    torch.save(retval["targets"], proprioception_target_file)
+    return retval            
+
+
 
 def load_multiview_demonstrations_as_proprioception_training(task, proprioception_input_file, proprioception_target_file, num_views=2):
     """
+
+    FIXME: Sahara: this needs to be changed to match the single-view one above. 
+
     Loads all the images of a task from multiple camera views, and processes it as two tensors
     as input and target data for proprioception training.
 
