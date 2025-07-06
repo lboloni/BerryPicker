b5e25e5fb82caae60c1e997284dc3eb127725292
Lotzi Bölöni <lotzi.boloni@gmail.com>
2025-07-06 12:21:06 -0400

    Continued work on supporting automation

--- src/automate.py
+++ src/automate.py
@@ -4,24 +4,33 @@ Config.PROJECTNAME = "BerryPicker"
 import pathlib
 import papermill as pm
 
-ext_path = pathlib.Path(Config()["experiment_external"])
+def automate_exprun(notebook, name, params):
+   """Automates the execution of a notebook. It is assumed that in the notebook there is cell tagged "parameters", and in general that the notebook is idempotent."""
 
-params = {}
-params["run"] = "sp_vae_256"
-params["external_path"] = str(pathlib.Path(ext_path, "_automate").resolve())
-params["epochs"] = 10
+   ext_path = pathlib.Path(Config()["experiment_external"])
+   params["external_path"] = str(pathlib.Path(ext_path, "_automate").resolve())
+   notebook_path = pathlib.Path(notebook)
+   output_path = pathlib.Path(ext_path, "_automation_output")
+   output_filename = f"{notebook_path.stem}_{name}_output{ notebook_path.suffix}"
+   output = pathlib.Path(output_path, notebook_path.parent, output_filename)
+   output.parent.mkdir(exist_ok=True)
+   print(output)
 
-print(params["external_path"])
+   try:
+      pm.execute_notebook(
+         notebook,
+         output.absolute(),
+         cwd=notebook_path.parent,
+         parameters=params
+      )
+   except Exception as e:
+      print(f"There was an exception {e}")
 
-output_path = pathlib.Path(ext_path, "_automation_output")
-output = pathlib.Path(output_path, "Train-Conv-VAE-output.ipynb")
+experiment = "automate"
+run = "automate_00"
+exp = Config().get_experiment(experiment, run)
 
-try:
-   pm.execute_notebook(
-      'sensorprocessing/Train-Conv-VAE.ipynb',
-      output.absolute(),
-      cwd="sensorprocessing",
-      parameters=params
-   )
-except Exception as e:
-   print(f"There was an exception {e}")
+for item in exp["exps_to_run"]:
+    print(f"***Automating {item['name']}")
+    #notebook = params["notebook"]
+    automate_exprun(item["notebook"], item["name"], item["params"])
