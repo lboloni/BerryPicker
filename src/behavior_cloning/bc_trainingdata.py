41d83d1ebd08322efadf4070f0a7c27b7dad6122
anthony <anthony.i.bilic@gmail.com>
2025-09-20 13:37:42 -0400

    merged Train_Conv_VAE as it is backward compatible. The same for Train_BehaviorCloning. Made shuffling and validation configurable. Both notebooks should perform their previous functions if configs are not changed.

--- src/behavior_cloning/bc_trainingdata.py
+++ src/behavior_cloning/bc_trainingdata.py
@@ -59,69 +59,102 @@ def create_trainingpair_prediction(x_seq: torch.Tensor, y_seq: torch.Tensor, seq
     return inputs_tensor, targets_tensor
 
 
-def create_trainingdata_bc(exp, exp_sp, exp_robot, device):
+def create_trainingdata_bc(exp, exp_sp, exp_robot, device, validation=False):
     """Creates training data for training and validation with the demonstrations specified in the exp/run. Caches the results into the input and target files specified in the exp/run. Remove those files to recalculate."""
+    # CHANGES
+    # Now it will not shuffle if configured not to, and it will create and use a validation split is configured to do so in exp
+
+
+    # Check if shuffle is defined in config
+    if 'shuffle' in exp:
+        shuffle = exp['shuffle']
+    else:
+        shuffle = True
 
     exp.start_timer("data_preparation")
 
-    input_path = pathlib.Path(exp.data_dir(), "training_input.pth")
-    target_path = pathlib.Path(exp.data_dir(), "training_target.pth")
+    # Converted to dictionaries to handle validation and repeat data creation loop
+    split_paths = {"training": 
+                   [pathlib.Path(exp.data_dir(), "training_input.pth"), 
+                    pathlib.Path(exp.data_dir(), "training_target.pth")],
+                    }
+    if validation:
+        split_paths['validation'] =  [pathlib.Path(exp.data_dir(), "validation_input.pth"), 
+                                      pathlib.Path(exp.data_dir(), "validation_target.pth")]
+                                
+    if not split_paths['training'][0].exists():
+        for key, value in split_paths.items():
+            input_path = value[0]
+            target_path = value[1]
 
-    if not input_path.exists():
-        all_demos_inputs_list = []
-        all_demos_targets_list = []
-        # Create the sp object described in the experiment
-        sp = create_sp(exp_sp, device)
-        transform = get_transform_to_sp(exp_sp)
-        for val in exp["training_data"]: # for all demonstrations
-            run, demo_name, camera = val
-            exp_demo = Config().get_experiment("demonstration", run)
-            demo = Demonstration(exp_demo, demo_name)
-            # read the a and z 
-            inputs_list = []
-            targets_list = []
-            for i in range(demo.metadata["maxsteps"]-1): # -1 because of lookahead
-                sensor_readings, _ = demo.get_image(i, device=device, transform=transform, camera=camera)                
-                # inputlist.append(sensor_readings[0])
-                z = sp.process(sensor_readings)
-                inputs_list.append(torch.from_numpy(z))
-                # the action we are choosing, is the next one
-                a = demo.get_action(i)
-                rp = RobotPosition.from_vector(exp_robot, a)
-                anorm = rp.to_normalized_vector(exp_robot)        
-                targets_list.append(torch.from_numpy(anorm))
-            inputs_tensor = torch.stack(inputs_list)
-            targets_tensor = torch.stack(targets_list)
-            # at this point we have two tensors that are aligned demonstrations
-            all_demos_inputs_tensor, all_demos_targets_tensor = create_trainingpair_prediction(inputs_tensor, targets_tensor, sequence_length=exp["sequence_length"])
-            all_demos_inputs_list.append(all_demos_inputs_tensor)
-            all_demos_targets_list.append(all_demos_targets_tensor)
-        all_demos_inputs_tensor = torch.cat(all_demos_inputs_list)
-        all_demos_targets_tensor = torch.cat(all_demos_targets_list)
-        # shuffle
-        perm = torch.randperm(all_demos_inputs_tensor.size(0))
-        all_demos_inputs_tensor = all_demos_inputs_tensor[perm]
-        all_demos_targets_tensor = all_demos_targets_tensor[perm]
-        # save
-        torch.save(all_demos_inputs_tensor, input_path)
-        torch.save(all_demos_targets_tensor, target_path)
-    else: # just load the cached data
-        all_demos_inputs_tensor = torch.load(input_path, weights_only=True)
-        all_demos_targets_tensor = torch.load(target_path, weights_only=True)
+            all_demos_inputs_list = []
+            all_demos_targets_list = []
+            # Create the sp object described in the experiment
+            sp = create_sp(exp_sp, device)
+            transform = get_transform_to_sp(exp_sp)
+            for val in exp["training_data"]: # for all demonstrations
+                run, demo_name, camera = val
+                exp_demo = Config().get_experiment("demonstration", run)
+                demo = Demonstration(exp_demo, demo_name)
+                # read the a and z 
+                inputs_list = []
+                targets_list = []
+                for i in range(demo.metadata["maxsteps"]-1): # -1 because of lookahead
+                    sensor_readings, _ = demo.get_image(i, device=device, transform=transform, camera=camera)                
+                    # inputlist.append(sensor_readings[0])
+                    z = sp.process(sensor_readings)
+                    inputs_list.append(torch.from_numpy(z))
+                    # the action we are choosing, is the next one
+                    a = demo.get_action(i)
+                    rp = RobotPosition.from_vector(exp_robot, a)
+                    anorm = rp.to_normalized_vector(exp_robot)        
+                    targets_list.append(torch.from_numpy(anorm))
+                inputs_tensor = torch.stack(inputs_list)
+                targets_tensor = torch.stack(targets_list)
+                # at this point we have two tensors that are aligned demonstrations
+                all_demos_inputs_tensor, all_demos_targets_tensor = create_trainingpair_prediction(inputs_tensor, targets_tensor, sequence_length=exp["sequence_length"])
+                all_demos_inputs_list.append(all_demos_inputs_tensor)
+                all_demos_targets_list.append(all_demos_targets_tensor)
+            demos_inputs_tensor = torch.cat(all_demos_inputs_list)
+            demos_targets_tensor = torch.cat(all_demos_targets_list)
+            # shuffle
+            if shuffle:
+                perm = torch.randperm(demos_inputs_tensor.size(0))
+                demos_inputs_tensor = demos_inputs_tensor[perm]
+                demos_targets_tensor = demos_targets_tensor[perm]
+            # save
+            torch.save(demos_inputs_tensor, input_path)
+            torch.save(demos_targets_tensor, target_path)
 
+    # Load from saved tensors
+    training_demos_inputs_tensor = torch.load(split_paths['training'][0])
+    training_demos_targets_tensor = torch.load(split_paths['training'][1])
+    # Load validation tensors
+    if validation:
+        validation_demos_inputs_tensor = torch.load(split_paths['validation'][0])
+        validation_demos_targets_tensor = torch.load(split_paths['validation'][1])
 
-    # split the training and validation data by first shuffling the pairs
-    # then separating them into the 2/3 - 1/3 ratio
-    rows = torch.randperm(all_demos_inputs_tensor.shape[0]) 
-    shuffled_inputs = all_demos_inputs_tensor[rows]
-    shuffled_targets = all_demos_targets_tensor[rows]
-
-    training_size = int( all_demos_inputs_tensor.shape[0] * 0.67 )
-
+    # shuffle training pairs
+    if shuffle:
+        rows = torch.randperm(training_demos_inputs_tensor.shape[0]) 
+        training_demos_inputs_tensor = training_demos_inputs_tensor[rows]
+        training_demos_targets_tensor = training_demos_targets_tensor[rows]
+    
     retval = {}
-    retval["z_train"] = shuffled_inputs[:training_size].to(device)
-    retval["a_train"] = shuffled_targets[:training_size].to(device)
-    retval["z_validation"] = shuffled_inputs[training_size:].to(device)
-    retval["a_validation"] = shuffled_targets[training_size:].to(device) 
+
+    # if no validation, separate training into the 2/3 - 1/3 ratio
+    if not validation:
+        training_size = int( training_demos_inputs_tensor.shape[0] * 0.67 )
+        retval["z_train"] = training_demos_inputs_tensor[:training_size].to(device)
+        retval["a_train"] = training_demos_targets_tensor[:training_size].to(device)
+        retval["z_validation"] = training_demos_inputs_tensor[training_size:].to(device)
+        retval["a_validation"] = training_demos_targets_tensor[training_size:].to(device) 
+    # if there is validation, then use training data as is and use validation data
+    else:
+        retval["z_train"] = training_demos_inputs_tensor.to(device)
+        retval["a_train"] = training_demos_targets_tensor.to(device)
+        retval["z_validation"] = validation_demos_inputs_tensor.to(device)
+        retval["a_validation"] = validation_demos_targets_tensor.to(device)         
+
     exp.end_timer("data_preparation")
     return retval
